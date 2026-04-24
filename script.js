0f386f321d16e0ebebe0fadbf2f2335170442bb3
XiomaraJimenezz <est.nicoll.jimenez@unimilitar.edu.co>
2026-04-24 12:53:46 -0500

    configuración: parámetros aleatorios del sistema

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ function plotPixel(ctx, x, y, color = "#000") {
     ctx.fillStyle = color;
     ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
 }
+/**
+ * Genera números aleatorios
+ */
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Parámetros principales
+const R = getRandom(100, 200);
+const N = getRandom(4, 10);
+const K = getRandom(3, 8);
+
+// Centro del canvas
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
