eb5f4af8ab1d4446fee6203f36d1d11ba6765fc9
XiomaraJimenezz <est.nicoll.jimenez@unimilitar.edu.co>
2026-04-24 12:51:53 -0500

    nueva funcionalidad: configuración inicial canvas y función plotPixel

--- script.js
+++ script.js
@@ -4,4 +4,14 @@
  * Estudiante: Nicoll Xiomara Jimenez Reyes
  * * Este código debe ser estructurado de forma modular.
  */
- 
+ // Obtener el canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+/**
+ * Función básica para pintar un píxel
+ */
+function plotPixel(ctx, x, y, color = "#000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
